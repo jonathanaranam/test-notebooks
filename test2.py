3968c0d22dc5f355a55fb628134d9d11a318caf2
Jonathan Arana Mora <jonathan.arana@handytec.mobi>
2020-05-14 22:39:55 -0500

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,6 +1,13 @@
-print('hola mundo')
-var test = ';'
-print(test)
-print(var)
-var var1 =+ test
-var var1 += test
+def sumay(x,y,z):
+    if x>z and y>z:
+        suma=x+y
+        return suma
+    elseif x>y and z>y:
+        suma=x+z
+        return suma
+    else:
+        suma=y+z
+        return suma
+
+total=sumay(10,11,12)
+print total
